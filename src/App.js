5bb11c0e37aab5b10dca0c96708e69d8ea9f18bb
username <vasanthkumars2025@gmail.com>
2023-03-09 08:26:32 +0530

    added navigation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,30 @@ import { Routes, Route } from 'react-router-dom'
 
 import Home from "./components/routes/home/home.component";
 
+const Navigation = () =>{
+  return (
+    <div>
+      <div>
+        <h1>I am the navigation bar</h1>
+      </div>
+    </div>
+  )
+}
+
+const Shop = () => {
+  return(
+    <div>
+      <h1>Shop page</h1>
+    </div>
+  )
+}
+
 const App = () => {
 
   return (
     <Routes>
-      <Route path='/' element={<Home />} >
+      <Route path='/home' element={<Home />} >
+        <Route path='shop' element={<Shop/>} />
         <Route />
       </Route>
     </Routes>
--- src/components/routes/home/home.component.jsx
+++ src/components/routes/home/home.component.jsx
@@ -1,3 +1,5 @@
+import { Outlet } from 'react-router-dom';
+
 import Directory from '../../directory/directory.component';
 
 const Home = () => {
@@ -33,6 +35,7 @@ const Home = () => {
   return (
     <div>
       <Directory categories={categories} />
+      <Outlet/>
     </div>
   );
 }
