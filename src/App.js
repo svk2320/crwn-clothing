aafe487bca9705802d14d05affaf9be7d6c0b455
username <vasanthkumars2025@gmail.com>
2023-03-09 12:21:57 +0530

    added sign in component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route} from 'react-router-dom'
 
 import Home from "./components/routes/home/home.component";
 import Navigation from './components/routes/navigation/navigation.component';
+import SignIn from './components/routes/sign-in/sign-in.component';
 
 const Shop = () => {
   return(
@@ -18,6 +19,7 @@ const App = () => {
       <Route path='/' element={<Navigation/>}>
         <Route index={true} element={<Home />} />
         <Route path='shop' element={<Shop/>} />
+        <Route path='sign-in' element={<SignIn/>} />
       </Route>
     </Routes>
   );
