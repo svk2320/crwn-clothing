63e030ad046eed629c0692928a3a0722af507f65
username <vasanthkumars2025@gmail.com>
2023-03-11 13:43:35 +0530

    added shop page and its data

--- src/App.js
+++ src/App.js
@@ -3,14 +3,7 @@ import { Routes, Route} from 'react-router-dom'
 import Home from "./components/routes/home/home.component";
 import Navigation from './components/routes/navigation/navigation.component';
 import Authentication from './components/routes/authentication/authentication.component';
-
-const Shop = () => {
-  return(
-    <div>
-      <h1>Shop page</h1>
-    </div>
-  )
-}
+import Shop from './components/routes/shop/shop.component';
 
 const App = () => {
 
