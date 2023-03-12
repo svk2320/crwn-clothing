d207d091fbec8ea64d3559634f9a4e87d54a5a87
username <vasanthkumars2025@gmail.com>
2023-03-12 08:38:02 +0530

    added feature for cart-dropdown to include details

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,10 +1,19 @@
 import { createContext, useState } from 'react';
 
 const addCartItem = (cartItems, productToAdd) => {
-  if (productToAdd in cartItems){
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+    );
 
-  } 
-}
+    if(existingCartItem) {
+      return cartItems.map((cartItem) => cartItem.id === productToAdd.id ?
+            {...cartItem, quantity: cartItem.quantity + 1} :
+            cartItem
+      )
+    };
+
+  return [...cartItems, { ...productToAdd, quantity: 1}]
+};
 
 export const CartContext = createContext({
   isCartOpen: false,
@@ -21,6 +30,6 @@ export const CartProvider = ({ children }) => {
     setCartItems(addCartItem(cartItems, productToAdd));
   }
 
-  const value = { isCartOpen, setIsCartOpen };
+  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
