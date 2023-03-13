6555d16a66d4815a9b174842bbdd5054566d1721
username <vasanthkumars2025@gmail.com>
2023-03-13 15:04:49 +0530

    used userReducer and installed in redux

--- src/contexts/user.contexts.jsx
+++ src/contexts/user.contexts.jsx
@@ -1,5 +1,7 @@
 import { createContext, useEffect, useReducer } from 'react';
 
+import { createAction } from '../services/reducer/reducer.services';
+
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
@@ -39,7 +41,7 @@ export const UserProvider = ({ children }) => {
   console.log(currentUser);
 
   const setCurrentUser = (user) => {
-    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload:user})
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user))
   }
   
   const value = { currentUser, setCurrentUser };
